d52e41364c21cc944fcd6354df4a01bf6a881568
arburty <austin@burt.us.com>
2020-08-06 18:21:13 -0600

    Ranger congigs to get it to display images in the same order in sxiv

---
+++ ranger/commands.py
@@ -0,0 +1,43 @@
+from __future__ import (absolute_import, division, print_function)
+
+from collections import deque
+import os
+import re
+
+from ranger.api.commands import Command
+
+# My cusomization: arburty
+class save_sorted_files_buffer(Command):
+    """:save_sorted_files_buffer
+
+    Save the sorted file buffer to datadir/copy_buffer
+    """
+    copy_sorted_filename = 'copy_sorted_files'
+
+    def execute(self):
+        import sys
+        fname = None
+        fname = self.fm.datapath(self.copy_sorted_filename)
+        unwritable = IOError if sys.version_info[0] < 3 else OSError
+        try:
+            fobj = open(fname, 'w')
+        except unwritable:
+            return self.fm.notify("Cannot open %s" %
+                                  (fname or self.copy_sorted_filename), bad=True)
+
+        startfile=self.fm.thisfile.path
+        # not being used currently
+        # fobj.write(startfile + "\n")
+        listoffiles = self.fm.thisdir.files
+        filenum=0
+        for o in listoffiles:
+            filenum += 1
+            if str(o.path) == startfile:
+                break
+
+        # fobj.write(listoffiles.index(startfile)) + "\n")
+        fobj.write(str(filenum) + "\n")
+        fobj.write("\n".join(fobj.path for fobj in listoffiles))
+        fobj.close()
+        return None
+
