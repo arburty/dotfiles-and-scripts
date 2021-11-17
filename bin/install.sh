c7c8b514ee58e98d257ae695419f7b5afa447d27
arburty <austin@burt.us.com>
2021-11-17 10:05:21 -0700

    install nerdfonts

--- bin/install.sh
+++ bin/install.sh
@@ -103,6 +103,11 @@ linkvimscriptsdir
 sudo apt-get install -y ffmpegthumbnailer fzf lynx mpv neovim pandoc ranger tmux tree w3m-img xterm vim-athena zathura zsh
 # }
 
+# install NerdFonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" \
+    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+
 # install Oh-My-Zsh Setup and Plugins {
 [[ ! -d $homedir/.oh-my-zsh/ ]] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
