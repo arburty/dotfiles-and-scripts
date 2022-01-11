4ea62094c9a7662c8dd4295ab6c09d662124f6b9
arburty <austin@burt.us.com>
2022-01-11 08:08:19 -0700

    fix minor bug with wsl_clip and update colorscheme colors

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -7,8 +7,15 @@ vim.cmd [[
 try
   " colorscheme darkplus
   colorscheme pop-punk
+  " a softer Visual Select. Defualt bright white
+  hi Visual ctermfg=148 ctermbg=240 guifg=#afdf00 guibg=#585858
+  " a red highlight
+  hi IncSearch ctermfg=15 ctermbg=161 guifg=#ffffff guibg=#d70061
+  hi Folded term=standout cterm=italic ctermfg=White ctermbg=236 gui=italic guifg=#a0a8b0 guibg=#384048
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme torte
   set background=dark
+  hi Folded term=standout cterm=italic ctermfg=14 ctermbg=236 gui=italic guifg=#a0a8b0 guibg=#384048
+  hi clear SignColumn      " SignColumn should match background
 endtry
 ]]
