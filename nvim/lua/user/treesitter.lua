7b4a28eb86795d4da29e7388b85aa72c3853e5ba
arburty <austin@burt.us.com>
2022-07-13 08:28:37 -0600

    Disable certain problem languages to make treesitter work nicely.

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -3,21 +3,42 @@ if not status_ok then
   return
 end
 
+require("nvim-treesitter.install").prefer_git = true
+
 configs.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    autopairs = {
-      enable = true,
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { 
+    "haskell",
+    "markdown",
+    "hack",
+    "help",
+    "embedded_template",
+    "proto",
+    "m68k",
+    "wgsl",
+    "elm",
+    "slint",
+    "fortran",
+    "verilog",
+    "org",
+    "astro",
+    "swift",
+    "scheme",
+    "todotxt",
+    "foam",
   },
-    highlight = {
-      enable = true, -- false will disable the whole extension
+  autopairs = {
+    enable = true,
+  },
+  highlight = {
+    enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
-    indent = { enable = true, disable = { "yaml" } },
-    context_commentstring = {
-      enable = true,
+  indent = { enable = true, disable = { "yaml" } },
+  context_commentstring = {
+    enable = true,
     enable_autocmd = false,
   },
 }
