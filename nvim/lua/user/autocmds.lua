1567b2750ad38c26aacdababf632605d531e93b6
arburty <austin@burt.us.com>
2022-08-23 08:38:31 -0600

    nvim add autocmd to fix gitconfig not being syntax highlighted.

--- nvim/lua/user/autocmds.lua
+++ nvim/lua/user/autocmds.lua
@@ -6,3 +6,10 @@ vim.api.nvim_create_autocmd({"BufRead"}, {
   group = cft,
   callback = function() vim.bo.filetype = "snote" end
 })
+
+local sft = vim.api.nvim_create_augroup("SetFileTypes", {clear = true})
+vim.api.nvim_create_autocmd({"BufRead"}, {
+  pattern = {".gitconfig","gitconfig"},
+  group = cft,
+  callback = function() vim.bo.filetype = "conf" end
+})
