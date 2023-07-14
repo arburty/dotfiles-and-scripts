b509274ee1f1829cf5ff1fd505588ef192130997
arburty <austin@burt.us.com>
2023-07-14 10:57:30 -0600

    get saveArgsToTmuxAndClip to work better on Mac.

--- bin/saveArgsToTmuxAndClip.sh
+++ bin/saveArgsToTmuxAndClip.sh
@@ -13,7 +13,7 @@ path="$HOME/.local/share/tmux"
 tmuxbuffer="$path/vim-selection.txt"
 
 # Strip newlines from end.
-content=$(echo -En "$@" | sed '${/^$/d} ; $s/\n//g')
+content=$(echo -En "$@" | sed '$s/\n//g')
 
 # Ensure path exists
 [ ! -d $path ] && mkdir -p $path
@@ -24,19 +24,14 @@ tmux load-buffer -b clipped $tmuxbuffer
 
 # Store in system clipboard.
 # The real powerhouse here
-if [[ $(command -v clip.exe) ]]
-then
-  clipcommand="clip.exe"
-elif [[ $(command -v xclip) ]]
-then
-  clipcommand="xclip -in -selection clipboard"
-elif [[ $(command -v pbcopy) ]]
-then
-  clipcommand="pbcopy"
-fi
+case "$machine" in
+  Linux)  clipcommand="xclip -in -selection clipboard";;
+  Mac)    clipcommand="pbcopy";;
+  WSL)    exit 1;;
+  *)      exit 2;;
+esac
 
 echo -En "$content" | $clipcommand
-
 
 
 ######### Vim function for reference. ############################
