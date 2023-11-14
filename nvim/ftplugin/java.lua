2a52b3b715088cfc5303fd20232d752f226e5bc8
arburty <austin@burt.us.com>
2023-11-14 16:31:59 -0700

    remove invesco java args

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -18,75 +18,40 @@ local bundles = {
 }
 vim.list_extend(bundles, vim.split(vim.fn.glob(home .. "/.local/share/nvim/vscode-java-test/server/*.jar", 1), "\n"))
 
+local java_jar = vim.fn.glob(home .. "/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_*.jar", 1)
+print("jj : " .. java_jar)
+
 local javaargs
 
-if mymachine == "WSL"
-then -- Invesco specific
-  local catalina_base = '/mnt/c/projects/' .. project_name .. '/.metadata/.plugins/org.eclipse.wst.server.core/tmp1'
-  local deploy = catalina_base .. '/wtpwebapps'
+javaargs = {
+  'java', -- or '/path/to/java11_or_newer/bin/java'
 
-  javaargs = {
-    --'java', -- or '/path/to/java11_or_newer/bin/java'
-    '/opt/jdk-17/bin/java',
+  '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+  '-Dosgi.bundles.defaultStartLevel=4',
+  '-Declipse.product=org.eclipse.jdt.ls.core.product',
+  '-Dlog.protocol=true',
+  '-Dlog.level=ALL',
+  '-Xms1g',
+  '--add-modules=ALL-SYSTEM',
+  '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+  '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
 
+  --[[ '--module-path', 'astro-modules/astro-dev.main', ]]
+  --[[ '--module-path', 'astro-dev/com.astro.dmp.dev.Application', ]]
+  --[[ '--module com.jenkov.mymodule/com.jenkov.mymodule.Main' ]]
+  '-Xmx1024m',
 
--- ==== GRADLE =======
-    '-Xmx1024m',
-    '-Djasypt.encryptor.password=Invesco123',
+  -- 💀
+  --[[ '-jar', home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.400.v20211117-0650.jar', ]]
+  --[[ '-jar', home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.2.400.v20211117-0650.jar', ]]
+  '-jar', java_jar,
+  -- 💀
+  --[[ '-configuration', './config_linux/config.ini', ]]
+  '-configuration', './config_mac/',
+  -- 💀
+  '-data', workspace_dir
+}
 
--- ==== MAGNOLIA =====
---    '${jrebel_args}',
---    '-Dcatalina.base=' .. catalina_base,
-    --'-Dcatalina.home=/mnt/c/usr/local/java/apache-tomcat-9.0.31',
---    '-Dwtp.deploy=' .. deploy,
-    --'--add-opens=java.base/java.lang=ALL-UNNAMED',
-    --'--add-opens=java.base/java.io=ALL-UNNAMED',
-    --'--add-opens=java.base/java.util=ALL-UNNAMED',
-    --'--add-opens=java.base/java.util.concurrent=ALL-UNNAMED',
-    --'--add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED',
-    --'--add-modules=ALL-SYSTEM',
--- ===== END =========
-
-    '-Dinvesco.ad.access=Inve5co',
-    '-Doauth.access=Invesco123',
-
-    -- 💀
-    '-jar', home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.400.v20211117-0650.jar',
-    -- 💀
-    '-configuration', './config_linux/config.ini',
-    -- 💀
-    '-data', workspace_dir
-  }
-else -- default args
-  javaargs = {
-    'java', -- or '/path/to/java11_or_newer/bin/java'
-
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
-    '--module-path', 'astro-modules/astro-dev.main',
-    --[[ '--module-path', 'astro-dev/com.astro.dmp.dev.Application', ]]
-    --[[ '--module com.jenkov.mymodule/com.jenkov.mymodule.Main' ]]
-    '-Xmx1024m',
-
-    -- 💀
-    --[[ '-jar', home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.400.v20211117-0650.jar', ]]
-    '-jar', home .. '/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.2.400.v20211117-0650.jar',
-    -- 💀
-    --[[ '-configuration', './config_linux/config.ini', ]]
-    '-configuration', './config_mac/',
-    -- 💀
-    '-data', workspace_dir
-  }
-
-end
 
 local config = {
   -- The command that starts the language server
