7b5acf616f2453a8901b38bc38540fd6fa3805db
arburty <austin@burt.us.com>
2023-11-21 15:15:55 -0700

    add my handler leader mappings for lsp

--- lvim/lua/user/keymaps.lua
+++ lvim/lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 
-print("Hello from LunarVim user keymaps")
+--[[ print("Hello from LunarVim user keymaps") ]]
 
 local source_keymaps = vim.api.nvim_create_augroup("source_keymaps", {clear = true})
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
@@ -87,3 +87,29 @@ keymap("n", "<leader>lI", "<cmd>Mason<cr>", opts)
 
 keymap("v", "<leader>a", "<Plug>(EasyAlign)", opts)
 keymap("v", "ga", "<Plug>(LiveEasyAlign)", opts)
+
+
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+--[[ keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) ]]
+keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+keymap("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+
+-- Not for DAP but still debugging AEM Java stuff.
+keymap('n', '<localleader>C', ':vs ~/aem-sdk/aem-curl/curl.sh<CR>', opts)
+keymap('n', '<localleader>c', ':vs | term ~/aem-sdk/aem-curl/curl.sh<cr>:norm i<CR>', opts)
+keymap('n', '<localleader>Z', ':vs ~/bin/rde-helper.zsh<cr>:norm i<CR>', opts)
+keymap('n', '<localleader>z', ':vs | term ~/bin/rde-helper.zsh<cr>:norm i<CR>', opts)
+
+keymap('n', '<M-j', '<nop>', opts)
+keymap('n', '<M-k', '<nop>', opts)
